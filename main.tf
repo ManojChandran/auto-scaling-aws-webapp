0a13629ee7b4e5b0577d1374fc0961eed1244bc7
Manoj Chandran <manoj.meparambu@gmail.com>
2023-06-10 23:12:49 +0530

    updated main vpc

--- main.tf
+++ main.tf
@@ -1,5 +1,31 @@
 #----root/main.tf-----
+terraform {
+  # To prevent automatic upgrades to new major versions 
+  # that may contain breaking changes
+  #--------------------------------------------------------------------------
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  ## Stores the state file back up in s3 bucket
+  ## interpolations cannot be used becasue the actvity is done initial stage
+  ## need to create s3 bucket and folder prior to using the backend
+  #--------------------------------------------------------------------------
+  #backend "s3" {
+  #  bucket = "myterraformstatebackupfile0002"
+  #  key    = "terraform/terraform.tfstate"
+  #  region = "us-east-1"
+  #}
+}
 
 provider "aws" {
-  region = "${var.aws_region}"
+  region = "${var.aws-region}"
+}
+
+# Deploy VPC and attach IGW
+module "vpc_igw" {
+  source   = "./modules/10_VPC_igw"
+  vpc-cidr = "${var.vpc-cidr}"
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,26 @@
-#----root/variables.tf-----
+#-----------------------------root/variables.tf-----------------------------
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#--------------------------------------------------------------------------
+variable "aws-region" {
+  description = "stored aws region data(N. Virginia)"
+  default = "us-east-1"
+}
 
-variable "aws_region" {}
+#variable "env" {
+#  description = "env: prod or test or dev"
+#}
+
+variable "vpc-cidr" {
+  description = "stores ip cidr for the VPC"
+  default = "10.0.0.0/26"
+}
